5d21891b55c2102f9400d2f8027f065ed32b6d3e
Tom Wilson <tom@hyper.io>
2022-01-22 13:30:33 +0000

    added static files to src dir

--- mod.ts
+++ mod.ts
@@ -2,9 +2,9 @@ import { serve, serveStatic } from 'server'
 import { graphql, shortcut } from './api.ts'
 
 serve({
-  '/': serveStatic("app.html", { baseUrl: import.meta.url }),
-  '/build/bundle.css': serveStatic("app.css", { baseUrl: import.meta.url }),
-  '/build/bundle.js': serveStatic("app.js", { baseUrl: import.meta.url }),
+  '/': serveStatic("src/app.html", { baseUrl: import.meta.url }),
+  '/build/bundle.css': serveStatic("src/app.css", { baseUrl: import.meta.url }),
+  '/build/bundle.js': serveStatic("src/app.js", { baseUrl: import.meta.url }),
   '/graphql': graphql,
   '/:code': async (_, params) => params
     ? Response.redirect(await shortcut(params.code))
